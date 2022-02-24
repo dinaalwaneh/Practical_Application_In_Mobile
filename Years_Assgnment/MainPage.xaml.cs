33f9018f94d31776cc2f079e0a7d712b247afb40
eleen <dbarahma8@gmail.com>
2022-02-24 18:23:12 +0300

    Display years from 1990-2030
    click at the button to display years :)

---
+++ Years_Assgnment/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Assignment3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayYears(object sender, EventArgs e)
+        {
+           int year = 1990;
+            for (; year <= 2030; year++)
+            {
+                Years.Text += year.ToString() + "\n";
+            }
+        }
+
+    }
+}
