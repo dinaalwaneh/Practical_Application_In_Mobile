c7957bdc73055d7b78dac8c718c79309fbc692ba
eleen <dbarahma8@gmail.com>
2022-02-24 19:40:14 +0300

    Click at the button to navigate a page displaying your information  :)

---
+++ Personal Data Assignment/Home.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+ 
+
+namespace App9
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new InfoPage());
+        }
+    }
+}
