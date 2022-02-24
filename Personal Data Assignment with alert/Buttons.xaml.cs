c75e003b58a1fce53630f7d6a9644681ec8fe3ec
eleen <dbarahma8@gmail.com>
2022-02-24 19:47:34 +0300

    Click at the button to display a message box with your information

---
+++ Personal Data Assignment with alert/Buttons.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Buttons : ContentPage
+    {
+
+        public Buttons()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            DisplayAlert("My Inforamtion", "Name : Dina Bader Alwaneh . \nBirthDate : 14-1-2002 . \nGender : Female . \nSpecialization : CAP .", "Close");
+        }
+    }
+}
